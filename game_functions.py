f76871bb1536b988fdd72ed2a39987e51f11de5d
handes <2451322819@qq.com>
2020-02-06 19:51:35 +0800

    2020/2/6

--- game_functions.py
+++ game_functions.py
@@ -14,9 +14,15 @@ def check_events(ship):
                 #向右移动飞船
                 # ship.rect.centerx += 1
                 ship.moving_right = True
+
+            if event.key == pygame.K_LEFT:
+                #向左移动飞船
+                ship.moving_left = True
         elif event.type == pygame.KEYUP:
             if event.key == pygame.K_RIGHT:
                 ship.moving_right = False
+            if event.key == pygame.K_LEFT:
+                ship.moving_left = False
 
 def update_screen(ai_settings, screen, ship):
     """更新屏幕上的图像，并切换到新屏幕"""
