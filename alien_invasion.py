264b09e1d4c6e8742c60c896c3380be26ccf0296
handes <2451322819@qq.com>
2020-02-06 19:36:55 +0800

    2020/2/6

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,9 +1,9 @@
-import sys
 
 import pygame
 
 from settings import Settings
 from ship import Ship
+import game_functions as gf
 
 def run_game():
     # 初始化游戏并创建一个屏幕对象
@@ -25,11 +25,8 @@ def run_game():
     # 开始游戏的主循环
     while True:
 
+        gf.check_events()
 
-        # 监视键盘额鼠标事件
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
 
         # 每次循环时都重绘屏幕
         screen.fill(ai_settings.bg_color)
