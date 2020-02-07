60343d7180cbcff17d14a2a7783a6152e7f4604f
handes <2451322819@qq.com>
2020-02-07 16:55:43 +0800

    2020/2/6

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,7 @@
 
 import pygame
 
+from pygame.sprite import Group
 from settings import Settings
 from ship import Ship
 import game_functions as gf
@@ -19,15 +20,19 @@ def run_game():
     # 设置背景色
     # bg_color = (230, 230, 230)
 
+    # 创建一个用于存储子弹的编组
+    bullets = Group()
+
     # 创建一艘飞船
     ship = Ship(ai_settings, screen)
 
     # 开始游戏的主循环
     while True:
 
-        gf.check_events(ship)
+        gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
-        gf.update_screen(ai_settings, screen, ship)
+        bullets.update()
+        gf.update_screen(ai_settings, screen, ship, bullets)
 
 
 
