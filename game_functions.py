aa07753ad660347ef035551166e35bdbe5064c96
handes <2451322819@qq.com>
2020-02-11 16:24:26 +0800

    2020/2/11

--- game_functions.py
+++ game_functions.py
@@ -49,6 +49,9 @@ def check_play_button(ai_settings, screen, stats, play_button, ship, aliens, bul
     button_clicked = play_button.rect.collidepoint(mouse_x, mouse_y)
     if button_clicked and not stats.game_active:
 
+        #重置游戏设置
+        ai_settings.initialize_dynamic_settings()
+
         #隐藏光标
         pygame.mouse.set_visible(False)
 
@@ -106,8 +109,9 @@ def check_bullet_alien_collisions(ai_settings, screen, ship, aliens, bullets):
     collisions = pygame.sprite.groupcollide(bullets, aliens, True, True)
 
     if len(aliens) == 0:
-        # 删除现有的子弹并新建一群外星人
+        # 删除现有的子弹并新建一群外星人,加快游戏节奏
         bullets.empty()
+        ai_settings.increase_speed()
         create_fleet(ai_settings, screen, ship, aliens)
 
 def fire_bullet(ai_settings, screen, ship, bullets):
